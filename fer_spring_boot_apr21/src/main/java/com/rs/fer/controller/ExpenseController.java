039ee4f3ca6977e90d2d41fe7b11bef5699e9e93
avinash <avinash@Lenovo-PC>
2021-09-27 17:41:40 +0530

    changes done in ExpenseController

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -5,14 +5,17 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.expense.request.AddExpenseRequest;
+import com.rs.fer.expense.request.DeleteExpenseRequest;
 import com.rs.fer.expense.request.EditExpenseRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
+import com.rs.fer.expense.response.DeleteExpenseResponse;
 import com.rs.fer.expense.response.EditExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
@@ -21,13 +24,9 @@ import com.rs.fer.expense.validation.ExpenseValidation;
 @RequestMapping("/api")
 public class ExpenseController {
 
-	
-	
 	@Autowired
 	ExpenseValidation expenseValidation;
 
-	
-	
 	@Autowired
 	ExpenseService expenseService;
 
@@ -59,6 +58,23 @@ public class ExpenseController {
 
 		} else {
 			response = expenseService.editExpense(request);
+		}
+
+		return response;
+	}
+
+	@DeleteMapping("/deleteExpense2")
+	public DeleteExpenseResponse deleteExpense2(@RequestBody DeleteExpenseRequest request) {
+
+		DeleteExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateDeleteExpenseRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new DeleteExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.deleteExpense(request);
 		}
 
 		return response;
