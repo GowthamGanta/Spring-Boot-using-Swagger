3dc4848a7e11f6635ab473143048adbed3bad0eb
GVR <GVR@DESKTOP-5VPSDDC>
2021-12-02 10:51:56 +0530

    Test for user1

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
@@ -9,6 +9,8 @@ public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-	  }    
+		
+		System.out.println("By user1");
+	}    
 
 }
