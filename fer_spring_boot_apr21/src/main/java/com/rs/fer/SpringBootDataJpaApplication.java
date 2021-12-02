d7c944b6d02b4b49bd38ac2b81d44ddbec11552b
91756 <91756@LAPTOP-BL9K43QJ>
2021-12-02 10:58:07 +0530

    Check ashok changes

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
@@ -10,7 +10,8 @@ public class SpringBootDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 		
-		System.out.println("By user1");
+		System.out.println("By user2");
+		System.out.println("ashok change");
 	}    
 
 }
