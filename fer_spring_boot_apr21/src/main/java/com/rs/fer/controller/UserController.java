d2e6d337fc81202a54caa4e7b0f472440fe31ae9
mukesh <mukesh@DESKTOP-5KAO57J>
2021-12-24 14:32:28 +0530

    resetpassword service integration

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -1,19 +1,25 @@
 package com.rs.fer.controller;
 
+import java.io.IOException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.user.request.LoginRequest;
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.request.ResetPasswordRequest;
 import com.rs.fer.user.response.LoginResponse;
 import com.rs.fer.user.response.RegistrationResponse;
+import com.rs.fer.user.response.ResetPasswordResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
 
@@ -68,6 +74,23 @@ public class UserController {
 		return response;
 	}
 
+	@PutMapping("/resetPassword")
+	public ResetPasswordResponse resetPassword(@RequestBody ResetPasswordRequest request) {
 
-	
+		ResetPasswordResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateResetPasswordRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new ResetPasswordResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		}
+
+		else {
+			response = userService.resetPassword(request);
+		}
+		System.out.println("Reset password....");
+		return response;
+	}
+
 }
