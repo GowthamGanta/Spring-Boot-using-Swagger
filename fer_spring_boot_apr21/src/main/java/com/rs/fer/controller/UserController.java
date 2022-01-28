a10a63a86b42704ae6ce8012c9027c4b262b6a46
GVR <GVR@DESKTOP-5VPSDDC>
2022-01-28 08:31:50 +0530

    Initial commit...

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -1,27 +1,17 @@
 package com.rs.fer.controller;
 
-import java.io.IOException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rs.fer.user.request.LoginRequest;
 import com.rs.fer.user.request.RegistrationRequest;
-import com.rs.fer.user.request.ResetPasswordRequest;
-import com.rs.fer.user.response.GetUserResponse;
-import com.rs.fer.user.response.LoginResponse;
 import com.rs.fer.user.response.RegistrationResponse;
-import com.rs.fer.user.response.ResetPasswordResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
 
@@ -36,11 +26,9 @@ import com.rs.fer.user.validation.UserValidation;
 @RequestMapping("/api")
 public class UserController {
 
-	
-	
 	@Autowired
 	UserValidation userValidation;
-	
+
 	@Autowired
 	UserService userService;
 
@@ -59,52 +47,5 @@ public class UserController {
 		}
 		return response;
 	}
-	
-	@PostMapping("/login")
-	public LoginResponse login(@RequestBody LoginRequest request) {
 
-		LoginResponse response = null;
-
-		Set<String> errorMessages = userValidation.validateLoginRequest(request);
-		// return response with error messages
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-			response = new LoginResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-
-		} else {
-			response = userService.login(request);
-		}
-		return response;
-	}
-
-	@PutMapping("/resetPassword")
-	public ResetPasswordResponse resetPassword(@RequestBody ResetPasswordRequest request) {
-
-		ResetPasswordResponse response = null;
-
-		Set<String> errorMessages = userValidation.validateResetPasswordRequest(request);
-
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-			response = new ResetPasswordResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-
-		}
-
-		else {
-			response = userService.resetPassword(request);
-		}
-		System.out.println("Reset password....");
-		return response;
-	}
-
-	@GetMapping("/getUser")
-	public GetUserResponse getUser(@RequestParam int userId) {
-
-		GetUserResponse response = null;
-		Set<String> errorMessages = userValidation.validateGetUserRequest(userId);
-
-		response = userService.getUser(userId);
-
-		System.out.println(userId);
-		
-		return response;
-	}
 }
