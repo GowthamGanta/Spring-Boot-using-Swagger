fe67f6e923cb4ada61b1650724c769f62b2100ee
Administrator <Administrator@DESKTOP-NUVT2Q2>
2022-01-28 13:00:52 +0530

    Ramadevi GetExpenseOptions Changes....

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.expense.request.AddExpenseRequest;
 import com.rs.fer.expense.request.DeleteExpenseRequest;
+import com.rs.fer.expense.request.GetExpensesRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
 import com.rs.fer.expense.response.DeleteExpenseResponse;
+import com.rs.fer.expense.response.GetExpensesResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
 
@@ -64,6 +67,23 @@ public class ExpenseController {
 
 		return response;
 	}
+	
+	@GetMapping("/getExpenseOptions")
 
+	public GetExpensesResponse getExpenseOptions(@ModelAttribute GetExpensesRequest request) {
 
+		GetExpensesResponse response = null;
+		Set<String> errorMessages = expenseValidation.validateGetExpensesRequest(request);
+
+		// return response with error message
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+
+			response = new GetExpensesResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+		} else {
+
+			response = expenseService.getExpenses(request);
+		}
+
+		return response;
+	}
 }
