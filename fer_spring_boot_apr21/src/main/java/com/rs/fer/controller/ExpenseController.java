25694de7d680172b039839c70b45552e0e419377
madhu <madhu@LAPTOP-UT5NV055>
2022-01-28 14:10:12 +0530

    ok

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -5,17 +5,13 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rs.fer.expense.request.AddExpenseRequest;
-import com.rs.fer.expense.request.DeleteExpenseRequest;
-import com.rs.fer.expense.response.AddExpenseResponse;
-import com.rs.fer.expense.response.DeleteExpenseResponse;
+import com.rs.fer.expense.request.GetExpensesRequest;
+import com.rs.fer.expense.response.GetExpensesResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
 
@@ -25,45 +21,24 @@ public class ExpenseController {
 
 	@Autowired
 	ExpenseValidation expenseValidation;
-	@Autowired
+	@Autowired 
 	ExpenseService expenseService;
 
-	@PostMapping("/ addExpense")
+	@GetMapping("/getExpensesReport")
+	public GetExpensesResponse getExpensesReport(@ModelAttribute GetExpensesRequest request) {
 
-	public AddExpenseResponse AddExpense(@ModelAttribute AddExpenseRequest request) {
+		GetExpensesResponse response = null;
 
-		AddExpenseResponse response = null;
-		Set<String> errorMessages = expenseValidation.validateAddExpenseRequest(request);
+		Set<String> errorMessages = expenseValidation.validateGetExpensesRequest(request);
 
-		// return response with error message
 		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new GetExpensesResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
 
-			response = new AddExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
 		} else {
-
-			response = expenseService.addExpense(request);
+			response = expenseService.getExpenses(request);
 		}
-
 		return response;
+
 	}
-	@DeleteMapping("/deleteExpense")
-
-	public DeleteExpenseResponse deleteExpense(@PathVariable DeleteExpenseRequest request) {
-
-		DeleteExpenseResponse response = null;
-		Set<String> errorMessages = expenseValidation.validateDeleteExpenseRequest(request);
-
-		// return response with error message
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-
-			response = new DeleteExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-		} else {
-
-			response = expenseService.deleteExpense(request);
-		}
-
-		return response;
-	}
-
 
 }
