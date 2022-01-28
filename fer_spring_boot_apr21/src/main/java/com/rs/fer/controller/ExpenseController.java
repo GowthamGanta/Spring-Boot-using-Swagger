80d2839b5e5d883a24f8ca452cf6acd7be9cc2aa
RAJIYA-PC <RAJIYA-PC@RAJIYA>
2022-01-28 12:11:49 +0530

    rajiya getExpense changes.....

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -1,6 +1,5 @@
 package com.rs.fer.controller;
 
-import java.io.IOException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.expense.request.AddExpenseRequest;
+import com.rs.fer.expense.request.GetExpenseRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
+import com.rs.fer.expense.response.GetExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
 
@@ -39,6 +40,25 @@ public class ExpenseController {
 		} else {
 
 			response = expenseService.addExpense(request);
+		}
+
+		return response;
+	}
+
+	@PostMapping("/getExpense")
+
+	public GetExpenseResponse getExpense(@ModelAttribute GetExpenseRequest request) {
+
+		GetExpenseResponse response = null;
+		Set<String> errorMessages = expenseValidation.validateGetExpenseRequest(request);
+
+		// return response with error message
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+
+			response = new GetExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+		} else {
+
+			response = expenseService.getExpense(request);
 		}
 
 		return response;
