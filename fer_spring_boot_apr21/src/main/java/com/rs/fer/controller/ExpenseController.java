d821ae206d21a86d87e3f3769159910201654cb9
LENOVO <LENOVO@Kongara-RK>
2022-02-25 13:21:10 +0530

    AddExpenses using @RequestBody changes are done.

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -8,6 +8,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,6 @@ import com.rs.fer.expense.request.AddExpenseRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
-import com.rs.fer.user.response.RegistrationResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -28,6 +28,24 @@ public class ExpenseController {
 	
 	@PutMapping("/addExpense")
 	public AddExpenseResponse addExpense(@ModelAttribute AddExpenseRequest request) {
+
+		AddExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateAddExpenseRequest(request);
+		
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new AddExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.addExpense(request);
+		}
+		return response;
+	}
+	
+	
+	@PostMapping("/addExpense")
+	public AddExpenseResponse addExpense1(@RequestBody AddExpenseRequest request) {
 
 		AddExpenseResponse response = null;
 
