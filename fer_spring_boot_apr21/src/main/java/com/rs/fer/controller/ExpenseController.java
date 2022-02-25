5ae8589fa02470ddba60b2efe169e1063809ffa2
LENOVO <LENOVO@Kongara-RK>
2022-02-25 15:01:52 +0530

    AddExpense changes done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -1,11 +1,26 @@
 package com.rs.fer.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.expense.request.AddExpenseRequest;
+import com.rs.fer.expense.request.DeleteExpenseRequest;
+import com.rs.fer.expense.response.AddExpenseResponse;
+import com.rs.fer.expense.response.DeleteExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
+import com.rs.fer.user.response.RegistrationResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +31,58 @@ public class ExpenseController {
 	@Autowired 
 	ExpenseService expenseService;
 	
+	@PutMapping("/addExpense")
+	public AddExpenseResponse addExpense(@ModelAttribute AddExpenseRequest request) {
+
+		AddExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateAddExpenseRequest(request);
+		
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new AddExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.addExpense(request);
+		}
+		return response;
+	}
 	
+	
+	@PostMapping("/addExpensma")
+	public AddExpenseResponse addExpensema(@RequestBody AddExpenseRequest request) {
+
+		AddExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateAddExpenseRequest(request);
+		
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new AddExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.addExpense(request);
+		}
+		return response;
+	}
+	
+
+	
+	
+	@DeleteMapping("/deleteExpense{expenseId}")
+	public DeleteExpenseResponse deleteExpense(@ModelAttribute DeleteExpenseRequest request) {
+
+		DeleteExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateDeleteExpenseRequest(request);
+		
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new DeleteExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.deleteExpense(request);
+		}
+		return response;
+	}
 }
