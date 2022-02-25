8344393ece73192eaa54760f8c381993cec644d1
DELL <DELL@QADARI>
2022-02-25 16:20:34 +0530

    ResetPassword is done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.user.request.LoginRequest;
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.request.ResetPasswordRequest;
 import com.rs.fer.user.request.UpdateUserRequest;
 import com.rs.fer.user.response.LoginResponse;
 import com.rs.fer.user.response.RegistrationResponse;
+import com.rs.fer.user.response.ResetPasswordResponse;
 import com.rs.fer.user.response.UpdateUserResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
@@ -88,6 +90,24 @@ public class UserController {
 		}
 		return response;
 	}
+	
+	@PutMapping("/resetPassword")
+	public ResetPasswordResponse resetPassword(@RequestBody ResetPasswordRequest request) {
+
+		ResetPasswordResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateResetPasswordRequest(request);
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new ResetPasswordResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = userService.resetPassword(request);
+		}
+		return response;
+	}
+	
+	
 }
 
 
