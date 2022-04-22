ec954e2a603fba446284a90c47a07e61036e3a3c
Nagaraju <stambalanagaraju99@gmail.com>
2022-04-22 13:59:55 +0530

    # WARNING: head commit changed in the meantime
    User registration based ModalAttribute Annotation

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -6,8 +6,10 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -45,8 +47,8 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@PostMapping("/registration")
-	public RegistrationResponse registration(@RequestBody RegistrationRequest request) {
+	@PostMapping("/registration/ma")
+	public RegistrationResponse registrationMA(@ModelAttribute RegistrationRequest request) {
 
 		RegistrationResponse response = null;
 
