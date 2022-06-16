41954a6c9ec037c986495f903d98df2c2cf67e5d
Pilli's PC <Pilli's PC@DESKTOP-CJBJ7Q6>
2022-06-16 13:59:46 +0530

    Get User

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/user/request/GetUserRequest.java
@@ -0,0 +1,14 @@
+package com.rs.fer.user.request;
+
+public class GetUserRequest {
+
+	private int userId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}
