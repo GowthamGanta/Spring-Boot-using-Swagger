42d5aad8d9bcbb85b66a0bc4a7d421260cbab5ad
Raghuram <Raghuram@DESKTOP-39ABHKV>
2022-06-16 11:54:35 +0530

    get expense options

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -5,30 +5,19 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rs.fer.entity.Expense;
 import com.rs.fer.expense.request.AddExpenseRequest;
 import com.rs.fer.expense.request.DeleteExpenseRequest;
-import com.rs.fer.expense.request.EditExpenseRequest;
-import com.rs.fer.expense.request.GetExpenseRequest;
 import com.rs.fer.expense.request.GetExpensesRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
 import com.rs.fer.expense.response.DeleteExpenseResponse;
-import com.rs.fer.expense.response.EditExpenseResponse;
-import com.rs.fer.expense.response.GetExpenseResponse;
 import com.rs.fer.expense.response.GetExpensesResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
-import com.rs.fer.user.request.RegistrationRequest;
-import com.rs.fer.user.response.RegistrationResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -71,4 +60,21 @@ public class ExpenseController {
 		}
 		return response;
 	}
+
+@PostMapping("/getExpenses")
+public GetExpensesResponse getExpenses(@RequestBody GetExpensesRequest request) {
+
+	GetExpensesResponse response = null;
+
+	Set<String> errorMessages = expenseValidation.validateGetExpensesRequest(request);
+
+	// return response with error messages
+	if (!CollectionUtils.isEmpty(errorMessages)) {
+		response = new GetExpensesResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+	} else {
+		response = expenseService.getExpenses(request);
+	}
+	return response;
+}
 }
