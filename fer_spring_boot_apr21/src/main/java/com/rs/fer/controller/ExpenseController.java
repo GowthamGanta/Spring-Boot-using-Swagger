9ed815141f36f6d9f41d6bb803cb1c6bad3e5bfa
sreen <sreen@Sree>
2022-07-15 18:49:57 +0530

    Get expense with requestbody....

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -139,7 +139,7 @@ public class ExpenseController {
 		return response;
 	}
 	
-	@GetMapping("/getExpense")
+	@PostMapping("/getExpense")
 	public GetExpenseResponse getExpenseRequest(@RequestBody GetExpenseRequest request) {
 		GetExpenseResponse response=null;
 		
