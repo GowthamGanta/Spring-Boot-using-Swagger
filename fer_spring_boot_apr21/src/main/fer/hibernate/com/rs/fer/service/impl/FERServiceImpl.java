eb67174f845c10be5d2266842357d868950ec2c0
GVR <GVR@DESKTOP-5VPSDDC>
2022-08-02 17:36:31 +0530

    Initial Commit...

--- fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/service/impl/FERServiceImpl.java
+++ fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/service/impl/FERServiceImpl.java
@@ -19,197 +19,71 @@ public class FERServiceImpl implements FERService {
 
 	@Override
 	public boolean registration(User user) {
-		boolean isRegister = false;
-		Session session = HibernateUtil.openSession();
-		Transaction transaction = session.beginTransaction();
-
-		int userId = (int) session.save(user);
-		isRegister = userId > 0;
-		transaction.commit();
-
-		session.close();
-
-		return isRegister;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public boolean login(String username, String password) {
-		boolean isValidUser = false;
-		Session session = HibernateUtil.openSession();
-
-		Criteria criteria = session.createCriteria(User.class);
-
-		criteria.add(Restrictions.eq("username", username));
-		criteria.add(Restrictions.eq("password", password));
-		List<User> users = criteria.list();
-		isValidUser = (users != null && !users.isEmpty());
-
-		return isValidUser;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public boolean addExpense(Expense expense);{
-
-
-        boolean isAddExpense = false;
-
-	       Session session = HibernateUtil.openSession();
-	        
-	       Transaction transaction = session.beginTransaction();
-	       int expenseId = (int) session.save(expense);
-	       isAddExpense = expenseId > 0;
-	       transaction.commit();
-	        
-	        session.close();
-	
-         return isAddExpense;
-
-
+	public boolean addExpense(com.rs.fer.entity.Expense expense) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-
-	public boolean editExpense(Expense expense) {
-		
-		boolean isEditedExpense = false;
-		
-		Session session = HibernateUtil.openSession();
-
-		try {
-			Transaction transaction = session.beginTransaction();
-			session.update(expense);
-			transaction.commit();
-
-			isEditedExpense = true;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
-		session.close();
-
-		return isEditedExpense;
-
+	public boolean editExpense(com.rs.fer.entity.Expense expense) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public boolean deleteExpense(int expenseId) {
-
-		boolean isDeletedExpense = false;
-
-		 Session session = HibernateUtil.openSession();
-			
-	        try {
-				Transaction transaction = session.beginTransaction();
-				Expense expense = new Expense();
-				expense.setId(expenseId);
-				session.delete(expense);
-				transaction.commit();
-				
-				isDeletedExpense = true;
-	        } catch (Exception ex) {
-	        	ex.printStackTrace();
-	        }
-			session.close();
-
-		
-		return isDeletedExpense;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public boolean resetPassword(int id, String currentPassword, String newPassword) {
-		
-		boolean isresetpassword= false;
-		
-		Session session = HibernateUtil.openSession();
-		Query query = session.createQuery("update User u set u.password=? where u.id=? and u.password=?");
-		query.setParameter(0, newPassword);
-		query.setParameter(1, id);
-		query.setParameter(2, currentPassword);
-		
-		
-		int numberOfRecAffected = query.executeUpdate();
-		isresetpassword = numberOfRecAffected > 0;
-
-        session.close();
-
-		return isresetpassword;	
+	public boolean resetPassword(int userId, String currentPassword, String newPassword) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public Expense getExpense(int expenseId) {
-		Expense expense = null;
-
-		return expense;
+	public com.rs.fer.entity.Expense getExpense(int expenseId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public List<Expense> getExpenseOptions(int userId) {
-
-		Session session = HibernateUtil.openSession();
-
-		Criteria criteria = session.createCriteria(Expense.class);
-		criteria.add(Restrictions.eq("userid", userid));
-
-		List<Expense> expenses = criteria.list();
-
-		session.close();
-
-		return expenses;
+	public List<com.rs.fer.entity.Expense> getExpenseOptions(int userId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public List<Expense> getExpenseReport(int userId, String expenseType, String fromDate, String toDate) {
-		
-		Session session = HibernateUtil.openSession();
-		
-		Criteria criteria = session.createCriteria(Expense.class);
-		
-		criteria.add(Restrictions.eq("userId", userId));
-		criteria.add(Restrictions.eq("expenseType", expenseType));
-		criteria.add(Restrictions.between("date", fromDate, toDate));
-		
-		List<Expense> expenseReport = criteria.list();
-		return expenseReport;
+	public List<com.rs.fer.entity.Expense> getExpenseReport(int userId, String expenseType, String fromDate,
+			String toDate) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public User getUser(int userId) {
-		User user = null;
-
-		
-	Session session = HibernateUtil.openSession();
-		
-		user= (User) session.get(User.class, userId);
-	    if(user.getAddress()==null) {
-	    	user.setAddress(new Address());
-	    }
-		
-		   session.close();
-		
-		
-		return user;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean updateUser(User user) {
-		boolean isUpdateUser = false;
-
-		Session session = HibernateUtil.openSession();
-		 try {
-	       Transaction transaction = session.beginTransaction();
-         session.update(user);
-	   
-	       transaction.commit();
-	       
-	       isUpdateUser=true;
-		 }catch(Exception ex) {
-			 ex.printStackTrace();
-		 }
-		   session.close();
-
-		
-		
-		return isUpdateUser;
+		// TODO Auto-generated method stub
+		return false;
 	}
+
+	
 
 }
