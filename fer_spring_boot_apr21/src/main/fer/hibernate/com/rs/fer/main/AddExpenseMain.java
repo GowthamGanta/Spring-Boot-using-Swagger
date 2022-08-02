78f94ded2cad4c676b6fcda7de97c749d27183f0
vinay045 <vinay045@vinay>
2022-08-02 19:28:05 +0530

    AddExpenseMain

--- fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/main/AddExpenseMain.java
+++ fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/main/AddExpenseMain.java
@@ -7,8 +7,8 @@ import com.rs.fer.service.impl.FERServiceImpl;
 public class AddExpenseMain {
 
 	public static void main(String[] args) {
-		
-		//1. Get the input
+
+		// 1. Get the input
 		Expense expense = new Expense();
 		expense.setType("dygsf");
 		expense.setDate("01-08-2022");
@@ -17,20 +17,17 @@ public class AddExpenseMain {
 		expense.setTotal(5000);
 		expense.setBywhom("Me");
 		expense.setUserId(4);
-		//2. Pass the input to service layer for business logic execution
-		FERService ferService = new FERServiceImpl();
+		// 2. Pass the input to service layer for business logic execution
+		FERService ferService = (FERService) new FERServiceImpl();
 		boolean isAddExpense = ferService.addExpense(expense);
-			
-		//3. Display the output 
-		if(isAddExpense) {
+
+		// 3. Display the output
+		if (isAddExpense) {
 			System.out.println("Expense Added successfuly......");
-		}else {
+		} else {
 			System.out.println("Expense Added is failed...");
 		}
-		
-		
 
 	}
 
 }
-
