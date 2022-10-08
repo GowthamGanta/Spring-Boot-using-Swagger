4594a8d096653a1057ab3a2fd26fcb503a00f50d
vallabhaneni latha <vallabhaneni latha@LAPTOP-DSO8N2EN>
2022-10-08 19:22:04 +0530

    editExpense code chnges by latha using model attribute

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.bean.Expense;
 import com.rs.fer.expense.request.AddExpenseRequest;
 import com.rs.fer.expense.request.EditExpenseRequest;
 import com.rs.fer.expense.response.AddExpenseResponse;
@@ -25,17 +26,18 @@ import com.rs.fer.user.response.RegistrationResponse;
 @RequestMapping("/api")
 public class ExpenseController {
 
+	private static final EditExpenseRequest request = null;
 	@Autowired
 	ExpenseValidation expenseValidation;
 	@Autowired
 	ExpenseService expenseService;
-	
+
 	@PostMapping("/addExpense")
 	public AddExpenseResponse registration(@RequestBody AddExpenseRequest request) {
 
 		AddExpenseResponse response = null;
 
-		Set<String> errorMessages =expenseValidation.validateAddExpenseRequest(request);
+		Set<String> errorMessages = expenseValidation.validateAddExpenseRequest(request);
 		// return response with error messages
 		if (!CollectionUtils.isEmpty(errorMessages)) {
 			response = new AddExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
@@ -46,7 +48,6 @@ public class ExpenseController {
 		return response;
 	}
 
-	
 	@PutMapping("/editExpense")
 	public EditExpenseResponse editExpense(@RequestBody EditExpenseRequest request) {
 
@@ -62,10 +63,9 @@ public class ExpenseController {
 		}
 		return response;
 	}
-	
 
-	@PutMapping("/editxepense")
-	public EditExpenseResponse editexpense(@ModelAttribute EditExpenseRequest request) {
+	@PutMapping("/editExpense1")
+	public EditExpenseResponse editExpense(@ModelAttribute Expense expense) {
 
 		EditExpenseResponse response = null;
 
@@ -79,5 +79,5 @@ public class ExpenseController {
 		}
 		return response;
 	}
-	
+
 }
