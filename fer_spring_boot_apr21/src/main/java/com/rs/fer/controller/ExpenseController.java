004a36991f95ca7525451b13639a6e65c67ca58b
Harsha <lilly@Harsha-PC>
2022-10-08 14:57:44 +0530

    edit expense code changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -1,11 +1,22 @@
 package com.rs.fer.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.expense.request.EditExpenseRequest;
+import com.rs.fer.expense.response.EditExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
+import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.response.RegistrationResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +27,23 @@ public class ExpenseController {
 	@Autowired
 	ExpenseService expenseService;
 
+	
+
+	@PutMapping("/editExpense")
+	public EditExpenseResponse editExpense(@RequestBody EditExpenseRequest request) {
+
+		EditExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateEditExpenseRequest(request);
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new EditExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.editExpense(request);
+		}
+		return response;
+	}
+	
 	
 }
