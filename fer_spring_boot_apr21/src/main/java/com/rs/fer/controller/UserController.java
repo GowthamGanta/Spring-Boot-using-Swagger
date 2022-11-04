5a54d50ae78503716c4d5079ccf74b7363602464
Chowdary <Chowdary@DESKTOP-UPM9MCQ>
2022-11-04 17:36:53 +0530

    Get User

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -2,6 +2,8 @@ package com.rs.fer.controller;
 
 import java.util.Set;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.user.request.GetUserRequest;
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.response.GetUserResponse;
 import com.rs.fer.user.response.RegistrationResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
@@ -33,7 +37,7 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
-  
+
 	@PostMapping("/registration")
 	public RegistrationResponse registration(@RequestBody RegistrationRequest request) {
 
@@ -46,6 +50,22 @@ public class UserController {
 
 		} else {
 			response = userService.registration(request);
+		}
+		return response;
+	}
+
+	@PostMapping("/getuser")
+	public GetUserResponse getuser(@PathParam(value = "") GetUserRequest request) {
+
+		GetUserResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateGetUserRequest(request);
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new GetUserResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = userService.getUser(request);
 		}
 		return response;
 	}
