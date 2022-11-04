62a32bc1a62016fd67749b4112d30c235cbc2921
harik <swathit@Swathi>
2022-11-04 18:10:53 +0530

    Get User

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -8,9 +8,12 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.user.request.GetUserRequest;
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.response.GetUserResponse;
 import com.rs.fer.user.response.RegistrationResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
@@ -46,6 +49,22 @@ public class UserController {
 
 		} else {
 			response = userService.registration(request);
+		}
+	   	return response;
+	}
+
+	@PostMapping("/getUser")
+	public GetUserResponse getUser(@RequestParam(value="userid" )GetUserRequest request) {
+
+		GetUserResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateGetUserRequest(request);
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new GetUserResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = userService.getUser(request);
 		}
 		return response;
 	}
