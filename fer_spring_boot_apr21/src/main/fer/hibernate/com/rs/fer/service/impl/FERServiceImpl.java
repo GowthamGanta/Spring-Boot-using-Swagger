47497094fcf1ac9724bbdae39f72fb81e8a79a6b
GVR <GVR@DESKTOP-5VPSDDC>
2022-12-01 11:58:09 +0530

    Reverting previous changes...

--- fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/service/impl/FERServiceImpl.java
+++ fer_spring_boot_apr21/src/main/fer/hibernate/com/rs/fer/service/impl/FERServiceImpl.java
@@ -1,193 +1,77 @@
 package com.rs.fer.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-
-import com.rs.fer.bean.Expense;
 import com.rs.fer.entity.User;
 import com.rs.fer.service.FERService;
-import com.rs.fer.util.HibernateUtil;
 
-public class FERServiceImpl implements FERService {
+public class FERServiceImpl  implements FERService {
 
 	@Override
 	public boolean registration(User user) {
-		boolean isRegister = true;
-
-		Session session = HibernateUtil.openSession();
-
-		Transaction transaction = session.beginTransaction();
-
-		isRegister = (int) session.save(user) > 0;
-
-		transaction.commit();
-		session.close();
-		return isRegister;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public int login(String username, String password) {
-		int userId = 0;
-
-		Session session = HibernateUtil.openSession();
-
-		Criteria criteria = session.createCriteria(User.class);
-		criteria.add(Restrictions.eq("username", username));
-		criteria.add(Restrictions.eq("password", password));
-
-		List<User> users = criteria.list();
-
-		if (users != null && !users.isEmpty()) {
-			userId = users.get(0).getId();
-
-		}
-		session.close();
-		return userId;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
-	public boolean addExpense(Expense expense) {
-		boolean isAddExpense = true;
-
-		Session session = HibernateUtil.openSession();
-
-		Transaction transaction = session.beginTransaction();
-
-		isAddExpense = (int) session.save(expense) > 0;
-
-		transaction.commit();
-		session.close();
-		return isAddExpense;
+	public boolean addExpense(com.rs.fer.entity.Expense expense) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public boolean editExpense(Expense expense) {
-		boolean isEditExpense = true;
-
-		Session session = HibernateUtil.openSession();
-		try {
-			Transaction transaction = session.beginTransaction();
-
-			session.update(expense);
-			transaction.commit();
-
-		} catch (Exception ex) {
-			isEditExpense = false;
-		}
-		session.close();
-
-		return isEditExpense;
+	public boolean editExpense(com.rs.fer.entity.Expense expense) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
-	public boolean deleteExpense(int expenseid) {
-		boolean isDeleted = true;
-		Session session = HibernateUtil.openSession();
-		try {
-			Transaction transaction = session.beginTransaction();
-			Expense expense = new Expense();
-			expense.setId(expenseid);
-			session.delete(expense);
-			transaction.commit();
-			
-		}catch(Exception ex){
-			isDeleted =false;
-		}
-		session.close();
-		return isDeleted;
+	public boolean deleteExpense(int expenseId) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-
+	@Override
 	public boolean resetPassword(int userId, String currentPassword, String newPassword) {
-		boolean isResetPassword = false;
-		
-		Session session = HibernateUtil.openSession();
-		Transaction transaction = session.beginTransaction();
-		
-		Query query = session.createQuery("update User u set u.password=? where u.id=? and u.password=?");
-		query.setParameter(0, newPassword);
-		query.setParameter(1, userId);
-		query.setParameter(2, currentPassword);
-		
-		isResetPassword = query.executeUpdate() > 0;
-		
-		transaction.commit();
-		session.close();
-		
-		return isResetPassword;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public com.rs.fer.entity.Expense getExpense(int expenseId) {
-		Expense expense = null;
-		
-		Session session = HibernateUtil.openSession();
-		expense = (Expense) session.get(Expense.class, expenseId);
-
-		session.close();
-		
-		return expense;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public List<Expense> getExpenseOptions(int userId) {
-		List<Expense> expenseoptions = null;
-		Session session = HibernateUtil.openSession();
-		Criteria criteria = session.createCriteria(Expense.class);
-		criteria.add(Restrictions.eq("userId", userId));
-
-		expenseoptions = criteria.list();
-		session.close();
-
-		return expenseoptions;
+	public List<com.rs.fer.entity.Expense> getExpenseOptions(int userId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public List<Expense> getExpenseReport(int userId, String expensetype, String fromdate, String todate) {
-		List<Expense> ExpenseReport = new ArrayList<>();
-		Session session = HibernateUtil.openSession();
-		Criteria criteria = session.createCriteria(User.class);
-		criteria.add(Restrictions.eq("userId", userId));
-		criteria.add(Restrictions.eq("type", expensetype));
-		criteria.add(Restrictions.between("date", fromdate, todate));
-		ExpenseReport = criteria.list();
-
-		return ExpenseReport;
+	public List<com.rs.fer.entity.Expense> getExpenseReport(int userId, String expenseType, String fromDate,
+			String toDate) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-
 
 	@Override
-	public User getUser(int userid) {
-		User user = null;
-		Session session = HibernateUtil.openSession();
-		user = (User) session.get(User.class, userid);
-
-		session.close();
-		return user;
+	public User getUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-
 
 	@Override
 	public boolean updateUser(User user) {
-		boolean isUpdateUser = false;
-		Session session = HibernateUtil.openSession();
-		try {
-			Transaction transaction = session.beginTransaction();
-			session.update(user);
-			transaction.commit();
-			
-
-		} catch (Exception ex) {
-			isUpdateUser = true;
-
-		}
-		session.close();
-		return isUpdateUser;
+		// TODO Auto-generated method stub
+		return false;
 	}
+	
 }
