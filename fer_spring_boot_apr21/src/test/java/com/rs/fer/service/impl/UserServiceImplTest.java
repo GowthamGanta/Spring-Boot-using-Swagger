74d547eb117561ae19b65170634c02e5cd16e108
GVR <GVR@DESKTOP-5VPSDDC>
2022-12-07 15:35:22 +0530

    Mockito...

---
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/service/impl/UserServiceImplTest.java
@@ -0,0 +1,131 @@
+package com.rs.fer.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.exceptions.base.MockitoException;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rs.fer.entity.User;
+import com.rs.fer.repository.UserRepository;
+import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.response.RegistrationResponse;
+import com.rs.fer.user.service.impl.UserServiceImpl;
+import com.rs.fer.user.util.UserUtil;
+
+@SpringBootTest
+public class UserServiceImplTest {
+
+	@InjectMocks
+	private UserServiceImpl userServiceImpl;
+	
+	@Mock
+	UserRepository userRepository;
+	
+	@Mock
+	UserUtil userUtil;
+	
+	@Test
+	public void testRegistration() {
+		
+		List<User> users = new ArrayList<>(1);
+		
+		User user = new User();
+		user.setUserId(1);
+		
+		//Mock		
+		when(userRepository.findByEmail(Mockito.anyString())).thenReturn(users);
+		
+		when(userRepository.save(Mockito.any())).thenReturn(user);
+		
+		when(userUtil.loadRegistrationRequestToUser(Mockito.any())).thenReturn(user);
+		
+		//1.
+		RegistrationRequest request = new RegistrationRequest();
+		request.setFirstname("admin");
+		request.setMiddlename("k");
+		request.setLastname("rs");
+		request.setEmail("admin@rs.com");
+		request.setUsername("admin");
+		request.setPassword("rs");
+		request.setMobile("2342343243");
+		
+		//2.
+		RegistrationResponse response = userServiceImpl.registration(request);
+		
+		//3.
+		assertEquals("000", response.statusCode);
+	}
+	
+	@Test
+	public void testRegistrationDuplicateEmail() {
+		
+		User user = new User();
+		user.setUserId(1);
+		
+		List<User> users = new ArrayList<>(1);
+		users.add(user);
+		
+		//Mock		
+		when(userRepository.findByEmail(Mockito.anyString())).thenReturn(users);
+		
+		when(userRepository.save(Mockito.any())).thenReturn(user);
+		
+		when(userUtil.loadRegistrationRequestToUser(Mockito.any())).thenReturn(user);
+		
+		//1.
+		RegistrationRequest request = new RegistrationRequest();
+		request.setFirstname("admin");
+		request.setMiddlename("k");
+		request.setLastname("rs");
+		request.setEmail("admin@rs.com");
+		request.setUsername("admin");
+		request.setPassword("rs");
+		request.setMobile("2342343243");
+		
+		//2.
+		RegistrationResponse response = userServiceImpl.registration(request);
+		
+		//3.
+		assertEquals("001", response.statusCode);
+	}
+	
+	@Test
+	public void testRegistrationFailure() {
+		
+		List<User> users = new ArrayList<>(1);
+		
+		User user = new User();
+		//user.setUserId(1);
+		
+		//Mock		
+		when(userRepository.findByEmail(Mockito.anyString())).thenReturn(users);
+		
+		when(userRepository.save(Mockito.any())).thenThrow(MockitoException.class);
+		
+		when(userUtil.loadRegistrationRequestToUser(Mockito.any())).thenReturn(user);
+		
+		//1.
+		RegistrationRequest request = new RegistrationRequest();
+		request.setFirstname("admin");
+		request.setMiddlename("k");
+		request.setLastname("rs");
+		request.setEmail("admin@rs.com");
+		request.setUsername("admin");
+		request.setPassword("rs");
+		request.setMobile("2342343243");
+		
+		//2.
+		RegistrationResponse response = userServiceImpl.registration(request);
+		
+		//3.
+		assertEquals("002", response.statusCode);
+	}
+}
