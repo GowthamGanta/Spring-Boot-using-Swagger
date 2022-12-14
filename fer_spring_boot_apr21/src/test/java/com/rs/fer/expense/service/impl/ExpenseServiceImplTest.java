543a7c9987f4cf7a48e9670e186ffa717a23d3b1
jammu <jammu@Pavithra>
2022-12-14 13:41:10 +0530

    addexpense mockito test cases

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/service/impl/ExpenseServiceImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/service/impl/ExpenseServiceImplTest.java
@@ -3,6 +3,7 @@ package com.rs.fer.expense.service.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.LinkedHashSet;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -13,9 +14,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rs.fer.expense.entity.Expense;
 import com.rs.fer.expense.repository.ExpenseRepository;
+import com.rs.fer.expense.request.AddExpenseRequest;
 import com.rs.fer.expense.request.DeleteExpenseRequest;
+import com.rs.fer.expense.response.AddExpenseResponse;
 import com.rs.fer.expense.response.DeleteExpenseResponse;
-import com.rs.fer.expense.service.impl.ExpenseServiceImpl;
+import com.rs.fer.expense.util.ExpenseUtil;
+import com.rs.fer.user.entity.User;
+import com.rs.fer.user.repository.UserRepository;
 
 @SpringBootTest
 public class ExpenseServiceImplTest {
@@ -25,6 +30,11 @@ public class ExpenseServiceImplTest {
 
 	@Mock
 	ExpenseRepository expenseRepository;
+	@Mock
+	ExpenseUtil expenseUtil;
+
+	@Mock
+	UserRepository userRepository;
 
 	@Test
 	public void testDeleteExpense() {
@@ -64,6 +74,76 @@ public class ExpenseServiceImplTest {
 
 		// 2.
 		DeleteExpenseResponse response = expenseServiceImpl.deleteExpense(request);
+
+		// 3.
+		assertEquals("002", response.statusCode);
+	}
+
+	@Test
+	public void testAddExpense() {
+
+		Expense expense = new Expense();
+		User user = new User();
+
+		user.setUserId(1);
+
+		user.setExpenses(new LinkedHashSet<Expense>());
+
+		Optional<User> userObj = Optional.of(user);
+
+		// User user = userObj.get();
+
+		when(userRepository.findById(Mockito.anyInt())).thenReturn(userObj);
+
+		when(expenseUtil.loadAddExpenseRequestToExpense(Mockito.any())).thenReturn(expense);
+
+		when(userRepository.save(Mockito.any())).thenReturn(user);
+
+		AddExpenseRequest request = new AddExpenseRequest();
+		request.setType("tea");
+		request.setDate("13/12/22");
+		request.setPrice(120);
+		request.setNumberOfItems(2);
+		request.setTotal(240);
+		request.setBywhom("me");
+		request.setUserId(1);
+
+		// 2.
+		AddExpenseResponse response = expenseServiceImpl.addExpense(request);
+
+		// 3.
+		assertEquals("000", response.statusCode);
+
+	}
+
+	@Test
+	public void testAddExpenseIdIsNotPresent() {
+
+		Optional<User> userObj = Optional.empty();
+
+		// Expense expense = new Expense();
+		// User user = null;
+
+		Expense expense = null;
+		// Mock
+		when(userRepository.findById(Mockito.anyInt())).thenReturn(userObj);
+
+		// when(userRepository.save(Mockito.any())).thenReturn(userObj);
+
+		when(expenseUtil.loadAddExpenseRequestToExpense(Mockito.any())).thenReturn(expense);
+
+		// 1.
+		AddExpenseRequest request = new AddExpenseRequest();
+		request.setType("tea");
+		request.setDate("13/12/22");
+		request.setPrice(120);
+		request.setNumberOfItems(2);
+		request.setTotal(240);
+		request.setBywhom("me");
+		request.setUserId(1);
+
+		// 2.
+		AddExpenseResponse response = expenseServiceImpl.addExpense(request);
 
 		// 3.
 		assertEquals("002", response.statusCode);
