83e6b964fb8b4604d05d002d785c8af5026de62c
jammu <jammu@Pavithra>
2022-12-16 14:48:30 +0530

    validate otp request and response

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/user/request/ValidateOtpRequest.java
@@ -0,0 +1,25 @@
+package com.rs.fer.user.request;
+
+public class ValidateOtpRequest {
+
+	private String id;
+
+	private String otp;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOtp() {
+		return otp;
+	}
+
+	public void setOtp(String otp) {
+		this.otp = otp;
+	}
+
+}
