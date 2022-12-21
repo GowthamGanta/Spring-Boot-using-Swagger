92ce18a6bf6f51fa61c2bda44fc11a8493a6f2a5
rajes <rajes@LAPTOP-D54OQPDI>
2022-12-21 14:43:14 +0530

    Admin Packages...

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/admin/service/AdminService.java
@@ -0,0 +1,15 @@
+package com.rs.fer.admin.service;
+
+import com.rs.fer.user.request.BlockUserRequest;
+import com.rs.fer.user.request.UnblockUserRequest;
+import com.rs.fer.user.response.BlockUserResponse;
+import com.rs.fer.user.response.UnblockUserResponse;
+
+public interface AdminService {
+
+	BlockUserResponse blockUser(BlockUserRequest request);
+
+	UnblockUserResponse unblockUser(UnblockUserRequest request);
+ 
+}
+ 
