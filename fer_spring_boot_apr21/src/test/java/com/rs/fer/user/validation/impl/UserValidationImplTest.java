0f221ec87e94c1fb162b75c47d51880a0c18c671
jammu <jammu@Pavithra.Dlink>
2022-12-24 11:58:29 +0530

    verifyEmail validation impl test cases...

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/user/validation/impl/UserValidationImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/user/validation/impl/UserValidationImplTest.java
@@ -10,16 +10,17 @@ import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.request.VerifyEmailRequest;
 
 @SpringBootTest
 public class UserValidationImplTest {
 
 	@InjectMocks
 	private UserValidationImpl userValidationImpl;
-	
+
 	@Test
 	public void testValidateRegistrationRequest() {
-		
+
 		RegistrationRequest request = new RegistrationRequest();
 		request.setFirstname("admin");
 		request.setLastname("rs");
@@ -27,34 +28,66 @@ public class UserValidationImplTest {
 		request.setUsername("admin");
 		request.setPassword("rs");
 		request.setMobile("23432432");
-		
+
 		Set<String> errorMessages = userValidationImpl.validateRegistrationRequest(request);
-		
+
 		boolean isExpectedEmpty = true;
 		boolean isActualEmpty = errorMessages.isEmpty();
-		
+
 		assertEquals(isExpectedEmpty, isActualEmpty);
-		
+
 	}
-	
+
 	@Test
 	public void testValidateRegistrationRequestFailure() {
-		
+
 		RegistrationRequest request = new RegistrationRequest();
-		//request.setFirstname("admin");
+		// request.setFirstname("admin");
 		request.setLastname("rs");
 		request.setEmail("admin@rs.com");
 		request.setUsername("admin");
 		request.setPassword("rs");
 		request.setMobile("23432432");
-		
+
 		Set<String> errorMessages = userValidationImpl.validateRegistrationRequest(request);
-		
+
 		boolean isExpectedEmpty = false;
 		boolean isActualEmpty = errorMessages.isEmpty();
-		
+
 		assertEquals(isExpectedEmpty, isActualEmpty);
-		
+
 	}
-	
+
+	@Test
+	public void testverifyEmailRequest() {
+
+		VerifyEmailRequest request = new VerifyEmailRequest();
+		request.setId("1");
+		request.setVerificationCode("p1a2v3i4t5h6r7a8");
+
+		Set<String> errorMessages = userValidationImpl.verifyEmailRequest(request);
+
+		boolean isExpectedEmpty = true;
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+
+	@Test
+	public void testverifyEmailRequestRequestFailure() {
+
+		VerifyEmailRequest request = new VerifyEmailRequest();
+		request.setId("2");
+		// request.setVerificationCode("p1a2v3i4t5h6r7a8");
+
+		Set<String> errorMessages = userValidationImpl.verifyEmailRequest(request);
+
+		boolean isExpectedEmpty = false;
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+
 }
