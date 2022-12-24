686d586c6cde80a996f1b6470238384ac0d543c5
jammu <jammu@Pavithra.Dlink>
2022-12-24 08:18:10 +0530

    AddExpense validation impl test cases.........

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/validation/impl/ExpenseValidationImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/validation/impl/ExpenseValidationImplTest.java
@@ -1,5 +1,64 @@
 package com.rs.fer.expense.validation.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rs.fer.expense.request.AddExpenseRequest;
+
+@SpringBootTest
 public class ExpenseValidationImplTest {
+
+	@InjectMocks
+	private ExpenseValidationImpl expenseValidationImpl;
+
+	@Test
+	public void testvalidateAddExpenseRequest() {
+
+		AddExpenseRequest request = new AddExpenseRequest();
+
+		request.setUserId(1);
+		request.setType("chocolate");
+		request.setDate("24/12/22");
+		request.setPrice(150);
+		request.setNumberOfItems(2);
+		request.setTotal(300);
+		request.setBywhom("me");
+
+		Set<String> errorMessages = expenseValidationImpl.validateAddExpenseRequest(request);
+
+		boolean isExpectedEmpty = true;
+
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+
+	@Test
+	public void testvalidateAddExpenseRequestFailure() {
+
+		AddExpenseRequest request = new AddExpenseRequest();
+
+		request.setUserId(1);
+		// request.setType("chocolate");
+		request.setDate("24/12/22");
+		request.setPrice(150);
+		request.setNumberOfItems(2);
+		request.setTotal(300);
+		request.setBywhom("me");
+
+		Set<String> errorMessages = expenseValidationImpl.validateAddExpenseRequest(request);
+
+		boolean isExpectedEmpty = false;
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
 
 }
