2053f864048e91842441e832d01e7dda61534e13
Hp <Hp@LavanyaLingisetti>
2022-12-24 15:10:23 +0530

    GetExpense Validation Test Cases....

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/validation/impl/ExpenseValidationImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/expense/validation/impl/ExpenseValidationImplTest.java
@@ -1,16 +1,18 @@
 package com.rs.fer.expense.validation.impl;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-import org.mockito.InjectMocks;
+import org.mockito.InjectMocks; 
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.rs.fer.expense.request.AddExpenseRequest;
+import com.rs.fer.expense.request.EditExpenseRequest;
+import com.rs.fer.expense.request.GetExpenseRequest;
 
 @SpringBootTest
 
@@ -50,15 +52,76 @@ public class ExpenseValidationImplTest {
 
 		request.setUserId(1);
 		// request.setType("chocolate");
+		request.setDate("24/12/22");  
+		request.setPrice(150);
+		request.setNumberOfItems(2);
+		request.setTotal(300);
+		request.setBywhom("me");
+  
+		Set<String> errorMessages = expenseValidationImpl.validateAddExpenseRequest(request);
+
+		boolean isExpectedEmpty = false;
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+	
+	
+	@Test
+	public void testvalidateGetExpenseRequest() {
+		
+		GetExpenseRequest request = new GetExpenseRequest();
+		
+		request.setExpenseId(1);  
+		
+		Set<String> errorMessages = expenseValidationImpl.validateGetExpenseRequest(request);
+		
+		boolean isExpectedEmpty = true;
+
+		boolean isActualEmpty = errorMessages.isEmpty();
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+		
+	}  
+	
+
+	@Test
+
+	public void testvalidateGetExpenseRequestFailure() {
+
+	GetExpenseRequest request = new GetExpenseRequest();
+	
+	request.setExpenseId(0);
+	
+	
+	Set<String> errorMessages = expenseValidationImpl.validateGetExpenseRequest(request);
+	
+	boolean isExpectedEmpty = false;
+	boolean isActualEmpty = errorMessages.isEmpty();
+	
+	assertEquals(isExpectedEmpty, isActualEmpty);  
+	  
+	
+	}
+	
+	@Test
+	public void testvalidateEditExpenseRequest() {
+
+		EditExpenseRequest request = new EditExpenseRequest();
+        request.setExpenseId(1);
+		request.setType("tea");
 		request.setDate("24/12/22");
 		request.setPrice(150);
 		request.setNumberOfItems(2);
 		request.setTotal(300);
 		request.setBywhom("me");
 
-		Set<String> errorMessages = expenseValidationImpl.validateAddExpenseRequest(request);
+		Set<String> errorMessages = expenseValidationImpl.validateEditExpenseRequest(request);
 
-		boolean isExpectedEmpty = false;
+		boolean isExpectedEmpty = true;
+
 		boolean isActualEmpty = errorMessages.isEmpty();
 
 		assertEquals(isExpectedEmpty, isActualEmpty);
