de851479168fd42064f75f485c61897757b55c99
jammu <jammu@Pavithra>
2022-12-26 10:49:03 +0530

    login and update user util test cases

---
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/user/util/impl/UserUtilImplTest.java
@@ -0,0 +1,122 @@
+
+package com.rs.fer.user.util.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rs.fer.user.entity.User;
+import com.rs.fer.user.request.LoginRequest;
+import com.rs.fer.user.request.UpdateUserRequest;
+import com.rs.fer.user.util.impl.UserUtilImpl;
+import com.rs.fer.user.validation.impl.UserValidationImpl;
+
+@SpringBootTest
+public class UserUtilImplTest {
+
+	@InjectMocks
+	private UserUtilImpl userUtilImpl;
+
+	@Test
+	public void testUtillLoginRequest() {
+
+		LoginRequest request = new LoginRequest();
+
+		request.setUsername("admin");
+		request.setPassword("rs");
+
+		User user = userUtilImpl.loadLoginRequestToUser(request);
+
+		boolean isExpectedNotEmpty = true;
+		boolean isActualNotEmpty = (null != user.getUsername() && null != user.getPassword());
+
+		assertEquals(isExpectedNotEmpty, isActualNotEmpty);
+
+	}
+
+	@Test
+	public void testUtillLoginRequestfailure() {
+
+		LoginRequest request = new LoginRequest();
+
+		// request.setUsername("admin");
+		request.setPassword("rs");
+
+		User user = userUtilImpl.loadLoginRequestToUser(request);
+
+		boolean isExpectedEmpty = true;
+		boolean isActualEmpty = (null == user.getUsername()) || (null == user.getPassword());
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+
+	@Test
+	public void testUpdateUserUtil() {
+
+		UpdateUserRequest request = new UpdateUserRequest();
+
+		request.setUserId(1);
+
+		request.setFirstname("admin");
+		request.setMiddlename("rs");
+		request.setLastname("suda");
+		request.setEmail("admin@rs.com");
+		request.setUsername("admin");
+		request.setPassword("rss");
+		request.setMobile("4545454545");
+
+		request.setAddressId(1);
+
+		request.setLineone("madhapur");
+		request.setLinetwo("100ft");
+		request.setCity("Hyd");
+		request.setState("TS");
+		request.setPincode("500018");
+		request.setCountry("IND");
+
+		User errorMessages = userUtilImpl.loadUpdateUserRequestToUser(request);
+
+		boolean isExpectedEmpty = true;
+		String isActualEmpty = errorMessages.getVerificationCode();
+
+		assertEquals(isExpectedEmpty, isExpectedEmpty);
+	}
+
+	@Test
+	public void testUpdateUserUtilfailure() {
+
+		UpdateUserRequest request = new UpdateUserRequest();
+
+		request.setUserId(1);
+
+		request.setFirstname("admin");
+		request.setMiddlename("rs");
+		request.setLastname("suda");
+		request.setEmail("admin@rs.com");
+		request.setUsername("admin");
+		request.setPassword("rss");
+		request.setMobile("4545454545");
+
+		request.setAddressId(1);
+
+		request.setLineone("madhapur");
+		request.setLinetwo("100ft");
+		request.setCity("Hyd");
+		request.setState("TS");
+		request.setPincode("500018");
+		request.setCountry("IND");
+
+		User errorMessages = userUtilImpl.loadUpdateUserRequestToUser(request);
+
+		boolean isExpectedEmpty = false;
+		String isActualEmpty = errorMessages.getVerificationCode();
+
+		assertEquals(isExpectedEmpty, isExpectedEmpty);
+	}
+
+}
