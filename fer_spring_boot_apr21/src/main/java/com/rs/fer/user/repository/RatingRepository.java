917077575b350758efb442d9c43552f61cfe00d6
bajibaba <baji156253700@gmail.com>
2022-12-27 08:42:37 +0530

    Rating Repository changes...

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/user/repository/RatingRepository.java
@@ -0,0 +1,18 @@
+package com.rs.fer.user.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rs.fer.user.entity.Rating;
+import com.rs.fer.user.entity.User;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Integer> {
+
+	List<Rating> findByUserIdAndReviewedBy(int userId, int reviewedById);
+
+	List<Rating> save(User userId);
+
+}
