7874e312c31591c945f0ed42ab23cc70f2780c31
shaik <shaik@LAPTOP-IIVNS1LO>
2023-01-27 18:41:35 +0530

    Edit Expense API changes done.

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/admin/controller/AdminController2.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/admin/controller/AdminController2.java
@@ -6,18 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rs.fer.admin.request.BlockUserRequest;
-import com.rs.fer.admin.request.UnblockUserRequest;
-import com.rs.fer.admin.response.BlockUserResponse;
-import com.rs.fer.admin.response.GetBlockUserResponse;
-import com.rs.fer.admin.response.GetUnblockUserResponse;
-import com.rs.fer.admin.response.UnblockUserResponse;
-import com.rs.fer.admin.service.AdminService;
-import com.rs.fer.admin.validation.AdminValidation;
+import com.rs.fer.expense.request.EditExpenseRequest;
+import com.rs.fer.expense.response.EditExpenseResponse;
+import com.rs.fer.expense.service.ExpenseService;
+import com.rs.fer.expense.validation.ExpenseValidation;
 import com.rs.fer.user.request.LoginRequest;
 import com.rs.fer.user.request.RegistrationRequest;
 import com.rs.fer.user.response.LoginResponse;
@@ -33,7 +30,12 @@ public class AdminController2 {
 	UserValidation userValidation;
 
 	@Autowired
+	ExpenseValidation expenseValidation;
+
+	@Autowired
 	UserService userService;
+	@Autowired
+	ExpenseService expenseService;
 
 	@PostMapping("/registration")
 	public RegistrationResponse registration(@RequestBody RegistrationRequest request) {
@@ -69,4 +71,21 @@ public class AdminController2 {
 		}
 		return response;
 	}
+
+	@PutMapping("/editExpense")
+	public EditExpenseResponse editExpense(@RequestBody EditExpenseRequest request) {
+
+		EditExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateEditExpenseRequest(request);
+		// return respnse with error messages
+		if (!org.springframework.util.CollectionUtils.isEmpty(errorMessages)) {
+			response = new EditExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+		} else {
+			response = expenseService.editExpense(request);
+		}
+		return response;
+
+	}
+
 }
