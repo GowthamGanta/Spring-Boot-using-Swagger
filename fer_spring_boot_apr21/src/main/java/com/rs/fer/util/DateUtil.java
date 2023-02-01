05ce10c198daa1a08811bac1d40f5429118d3141
tullu <tullu@DESKTOP-5C703BT>
2023-02-01 08:50:32 +0530

    DateUtil code changes for message API

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/util/DateUtil.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/util/DateUtil.java
@@ -6,10 +6,13 @@ import java.util.Date;
 public class DateUtil {
 
 	public static String getCurrentDate() {
-		
 		SimpleDateFormat  simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		return simpleDateFormat.format(new Date());
 	}
 
+	public static String getCurrentDateAndTime() {
+		SimpleDateFormat  simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return simpleDateFormat.format(new Date());
+	}
 	
 }
