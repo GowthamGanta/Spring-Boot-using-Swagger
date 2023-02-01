eacb4ee59b02283f6682268d50f1b4a7e325ed73
DELL <DELL@VamsiCr7>
2023-02-01 08:29:19 +0530

    code changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/util/DateUtil.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/util/DateUtil.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class DateUtil {
 
 	public static String getCurrentDate() {
+		
 		SimpleDateFormat  simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		return simpleDateFormat.format(new Date());
 	}
