96196b81062cdc85d93df19829cbffa3833b3bcf
tullu <tullu@DESKTOP-5C703BT>
2023-02-02 10:23:25 +0530

    getRequest, getResponse code changes for message API

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/message/request/GetMessagesRequest.java
@@ -0,0 +1,24 @@
+package com.rs.fer.message.request;
+
+public class GetMessagesRequest {
+
+	private int messageTheradId;
+	private int userdId;
+
+	public int getMessageTheradId() {
+		return messageTheradId;
+	}
+
+	public void setMessageTheradId(int messageTheradId) {
+		this.messageTheradId = messageTheradId;
+	}
+
+	public int getUserdId() {
+		return userdId;
+	}
+
+	public void setUserdId(int userdId) {
+		this.userdId = userdId;
+	}
+
+}
---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/message/response/GetMessagesResponse.java
@@ -0,0 +1,33 @@
+package com.rs.fer.message.response;
+
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+
+import com.rs.fer.message.entity.Message;
+import com.rs.fer.response.AbstractResponse;
+
+public class GetMessagesResponse extends AbstractResponse {
+
+	public GetMessagesResponse(HttpStatus status, String statusCode, String message, Set<String> errorMessages) {
+
+		super(status, statusCode, message, errorMessages);
+
+		this.status = status;
+		this.statusCode = statusCode;
+		this.message = message;
+		this.errorMessages = errorMessages;
+
+	}
+
+	private Message messages;
+
+	public Message getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Message messages) {
+		this.messages = messages;
+	}
+
+}
