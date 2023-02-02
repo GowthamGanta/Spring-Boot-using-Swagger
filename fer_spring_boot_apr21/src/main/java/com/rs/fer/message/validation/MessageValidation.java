c55f18a369762d2f1bdae4f722cdbf48ea51131b
ranit <ranit@Rani>
2023-02-02 11:22:06 +0530

    validation code changes for message api

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/message/validation/MessageValidation.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/message/validation/MessageValidation.java
@@ -2,10 +2,12 @@ package com.rs.fer.message.validation;
 
 import java.util.Set;
 
+import com.rs.fer.message.request.GetMessagesRequest;
 import com.rs.fer.message.request.SaveMessageRequest;
 
 public interface MessageValidation {
 
 	Set<String> validateSaveMessage(SaveMessageRequest request);
 
+	Set<String> validateGetMessages(GetMessagesRequest request);
 }
--- fer_spring_boot_apr21/src/main/java/com/rs/fer/message/validation/impl/MessageValidationImpl.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/message/validation/impl/MessageValidationImpl.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
+import com.rs.fer.message.request.GetMessagesRequest;
 import com.rs.fer.message.request.SaveMessageRequest;
 import com.rs.fer.message.validation.MessageValidation;
 import com.rs.fer.util.FERUtil;
@@ -20,6 +21,18 @@ public class MessageValidationImpl implements MessageValidation {
 		errorMessages = FERUtil.addErrorIfEmpty(errorMessages, request.getMessage(), "Please enter message");
 		errorMessages = FERUtil.addErrorIfEmpty(errorMessages, request.getSenderId(), "Please enter senderId");
 		errorMessages = FERUtil.addErrorIfEmpty(errorMessages, request.getReceiverId(), "Please enter receiverId");
+
+		return errorMessages;
+	}
+
+	@Override
+	public Set<String> validateGetMessages(GetMessagesRequest request) {
+
+		Set<String> errorMessages = new LinkedHashSet<String>();
+
+		errorMessages = FERUtil.addErrorIfEmpty(errorMessages, request.getMessageTheradId(),
+				"Please enter messageThreadId");
+		errorMessages = FERUtil.addErrorIfEmpty(errorMessages, request.getUserdId(), "Please enter UserId");
 
 		return errorMessages;
 	}
