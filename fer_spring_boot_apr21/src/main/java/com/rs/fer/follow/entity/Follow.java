2f0c816d1824860d20d68d8134099a81782c9a0f
shiva parvathi <shiva parvathi@LAPTOP-BOBDM41Q>
2023-03-06 09:11:40 +0530

    Follower Entity changes

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/entity/Follow.java
@@ -0,0 +1,78 @@
+package com.rs.fer.follow.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="follow")
+public class Follow {
+	@Id
+	@Column(name = "follow_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private int id;
+
+	@Column
+	private int userId;
+	@Column
+	private int followerId;
+	@Column
+	private String created;
+	@Column
+	private String updated;
+
+	public Follow() {
+
+	}
+
+	public Follow(int userId, int followerId) {
+		this.followerId = followerId;
+		this.userId = userId;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getFollowerId() {
+		return followerId;
+	}
+
+	public void setFollowerId(int followerId) {
+		this.followerId = followerId;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+
+}
