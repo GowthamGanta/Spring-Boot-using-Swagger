d57b9a5bf4f810284ba3bfa25a81af4666eee9c0
Uday <Uday@LAPTOP-0NLBI4T0>
2023-03-06 22:48:56 +0530

    validation test

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/validation/FollowValidation.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/validation/FollowValidation.java
@@ -3,7 +3,6 @@ package com.rs.fer.follow.validation;
 import java.util.Set;
 
 import com.rs.fer.follow.request.SaveFollowerRequest;
-import com.rs.fer.message.request.SaveMessageRequest;
 
 public interface FollowValidation {
 	
