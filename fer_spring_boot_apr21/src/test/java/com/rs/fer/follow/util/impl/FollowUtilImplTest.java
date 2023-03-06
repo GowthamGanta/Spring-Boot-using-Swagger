e2f339935cebed8e8bc6a00f61547c9fb5f4c984
Admin <Admin@DESKTOP-RB2U3HU>
2023-03-06 21:40:47 +0530

    Util Test Cases are done

---
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/follow/util/impl/FollowUtilImplTest.java
@@ -0,0 +1,48 @@
+package com.rs.fer.follow.util.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rs.core.follow.util.impl.FollowUtilImpl;
+import com.rs.fer.follow.entity.Follow;
+import com.rs.fer.follow.request.SaveFollowerRequest;
+
+@SpringBootTest
+public class FollowUtilImplTest {
+
+	@InjectMocks
+	private FollowUtilImpl followUtilImpl;
+
+	@Test
+	public void loadSaveFollowRequest() {
+
+		SaveFollowerRequest request = new SaveFollowerRequest();
+		request.setFollowerId(1);
+		request.setUserId(2);
+
+		Follow follow = followUtilImpl.loadSaveFollowerRequest(request, 1, 2);
+		boolean isExpectedEmpty = true;
+		boolean isActualEmpty = (0 != follow.getFollowerId() && 0 != follow.getUserId());
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+
+	@Test
+	public void loadSaveFollowRequestFailure() {
+
+		SaveFollowerRequest request = new SaveFollowerRequest();
+		//request.setFollowerId(1);
+		request.setUserId(2);
+
+		Follow follow = followUtilImpl.loadSaveFollowerRequest(request, 1, 2);
+		boolean isExpectedEmpty = false;
+		boolean isActualEmpty = (0 != follow.getFollowerId() && 0 != follow.getUserId());
+
+		assertEquals(isExpectedEmpty, isActualEmpty);
+
+	}
+}
