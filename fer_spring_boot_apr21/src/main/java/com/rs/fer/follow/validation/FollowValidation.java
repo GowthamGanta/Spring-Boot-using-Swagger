0af17d8f2912a8f3ef1e1c8112291991858bee2f
Uday <Uday@LAPTOP-0NLBI4T0>
2023-03-07 12:41:31 +0530

    Validations Done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/validation/FollowValidation.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/validation/FollowValidation.java
@@ -2,10 +2,12 @@ package com.rs.fer.follow.validation;
 
 import java.util.Set;
 
+import com.rs.fer.follow.request.DeletefollowerRequest;
 import com.rs.fer.follow.request.SaveFollowerRequest;
 
 public interface FollowValidation {
-	
+
 	Set<String> validateSaveFollowerRequest(SaveFollowerRequest request);
 
+	Set<String> validateDeleteFollowerRequest(DeletefollowerRequest request);
 }
