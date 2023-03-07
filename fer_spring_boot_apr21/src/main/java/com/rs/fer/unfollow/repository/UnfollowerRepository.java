b360c11265282992569200fb1cb5622eed418ee5
Admin <Admin@DESKTOP-RB2U3HU>
2023-03-07 12:35:04 +0530

    Repository changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/unfollow/repository/UnfollowerRepository.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/unfollow/repository/UnfollowerRepository.java
@@ -9,4 +9,5 @@ import com.rs.fer.follow.entity.Follow;
 public interface UnfollowerRepository extends JpaRepository<Follow, Integer> {
 
 	List<Follow> findByUserIdAndFollowerId(int userId, int followerId);
+	
 }
