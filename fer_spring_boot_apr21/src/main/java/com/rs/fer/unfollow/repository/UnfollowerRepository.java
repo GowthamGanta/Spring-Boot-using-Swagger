47c0086962c809c5e2b51d5aa0f96a789b0e8a0f
Admin <Admin@DESKTOP-RB2U3HU>
2023-03-07 10:33:22 +0530

    Repository done

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/unfollow/repository/UnfollowerRepository.java
@@ -0,0 +1,13 @@
+package com.rs.fer.unfollow.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rs.fer.follow.entity.Follow;
+
+public interface UnfollowerRepository extends JpaRepository<Follow, Integer> {
+
+	List<Follow> findByUserIdAndFollowerId(int userId, int followerId);
+
+}
