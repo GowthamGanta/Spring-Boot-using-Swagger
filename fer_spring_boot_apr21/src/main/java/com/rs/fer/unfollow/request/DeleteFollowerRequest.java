98448d601d0b926b01f849b7dcc0a7006b0bc96a
DELL E7390 <DELL E7390@DESKTOP-C2I84TP>
2023-03-07 11:01:30 +0530

    Delete Follower Request and Response

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/unfollow/request/DeleteFollowerRequest.java
@@ -0,0 +1,19 @@
+package com.rs.fer.unfollow.request;
+
+public class DeleteFollowerRequest {
+	private int userId;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getFollowerId() {
+		return followerId;
+	}
+	public void setFollowerId(int followerId) {
+		this.followerId = followerId;
+	}
+	private int followerId;
+
+}
