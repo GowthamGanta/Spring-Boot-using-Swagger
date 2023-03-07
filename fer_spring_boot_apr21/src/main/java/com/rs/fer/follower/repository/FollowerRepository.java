ab60a9474da487ff7eef71a081a5120ab47ef89d
ADMIN <ADMIN@DESKTOP-321N3D7>
2023-03-07 16:16:12 +0530

    changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/follower/repository/FollowerRepository.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/follower/repository/FollowerRepository.java
@@ -1,10 +1,14 @@
 package com.rs.fer.follower.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rs.fer.follow.entity.Follow;
 
 
 public interface FollowerRepository extends JpaRepository<Follow, Integer> {
+
+	List<Follow> findByUserIdAndFollowerId(int userId, int followerId);
 
 }
