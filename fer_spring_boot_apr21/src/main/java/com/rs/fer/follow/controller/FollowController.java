e8f56ca34f11d5a453aa541356d879d3e94b98e3
Uday <Uday@LAPTOP-0NLBI4T0>
2023-03-08 14:21:09 +0530

    controller changes done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/controller/FollowController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/follow/controller/FollowController.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.follow.request.DeletefollowerRequest;
+import com.rs.fer.follow.request.GetFollowersRequest;
 import com.rs.fer.follow.request.SaveFollowerRequest;
 import com.rs.fer.follow.response.DeletefollowerResponse;
+import com.rs.fer.follow.response.GetFollowersResponse;
 import com.rs.fer.follow.response.SaveFollowerResponse;
 import com.rs.fer.follow.service.FollowerService;
 import com.rs.fer.follow.validation.FollowValidation;
@@ -59,4 +62,21 @@ public class FollowController {
 
 		return response;
 	}
+
+	@GetMapping("/getFollowers")
+	public GetFollowersResponse getFollowers(@RequestBody GetFollowersRequest request) {
+
+		GetFollowersResponse response = null;
+		Set<String> errorMessges = followValidation.validateGetFollowersRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessges)) {
+			response = new GetFollowersResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessges);
+
+		} else {
+			response = followerService.getFollowers(request);
+		}
+
+		return response;
+	}
+
 }
