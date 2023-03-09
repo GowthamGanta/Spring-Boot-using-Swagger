0f33ee33b71ad858576c544384fb3c5214ec2bf9
DELL E7390 <DELL E7390@DESKTOP-C2I84TP>
2023-03-09 17:49:17 +0530

    Code Changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/util/ParticipantUtil.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/util/ParticipantUtil.java
@@ -1,10 +1,10 @@
 package com.rs.fer.participant.util;
 
 import com.rs.fer.participant.entity.Participant;
-import com.rs.fer.participant.request.SavePartcipantRequest;
+import com.rs.fer.participant.request.SaveParticipantRequest;
 
 public interface ParticipantUtil {
 	
-	Participant loadSaveParticipantRequest(SavePartcipantRequest request, int participantId, int groupId);
+	Participant loadSaveParticipantRequest(SaveParticipantRequest request, int participantId, int groupId);
 
 }
--- fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/util/impl/ParticipantUtilImpl.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/util/impl/ParticipantUtilImpl.java
@@ -3,14 +3,14 @@ package com.rs.fer.participant.util.impl;
 import org.springframework.stereotype.Component;
 
 import com.rs.fer.participant.entity.Participant;
-import com.rs.fer.participant.request.SavePartcipantRequest;
+import com.rs.fer.participant.request.SaveParticipantRequest;
 import com.rs.fer.participant.util.ParticipantUtil;
 
 @Component
 public class ParticipantUtilImpl implements ParticipantUtil {
 
 	@Override
-	public Participant loadSaveParticipantRequest(SavePartcipantRequest request, int participantId, int groupId) {
+	public Participant loadSaveParticipantRequest(SaveParticipantRequest request, int participantId, int groupId) {
 		
 		Participant participants = new Participant();
 		participants.setParticipantId(request.getParticipantId());
