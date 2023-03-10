b72ea46589e169171c0815f32987555b458bb5fd
Admin <Admin@DESKTOP-RB2U3HU>
2023-03-10 15:52:39 +0530

    changes are done

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/request/DeleteParticipantRequest.java
@@ -0,0 +1,15 @@
+package com.rs.fer.participant.request;
+
+public class DeleteParticipantRequest {
+	
+	private int participantId;
+
+	public int getParticipantId() {
+		return participantId;
+	}
+
+	public void setParticipantId(int participantId) {
+		this.participantId = participantId;
+	}
+
+}
