7d44c291e932b54bf18a432b93688a512ce61de6
shiva parvathi <shiva parvathi@LAPTOP-BOBDM41Q>
2023-03-13 11:31:50 +0530

    Changes are done

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/repository/ParticipantRepository.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/participant/repository/ParticipantRepository.java
@@ -1,5 +1,8 @@
 package com.rs.fer.participant.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.rs.fer.participant.Participant;
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 
+	List<Participant> findByGroupId(int groupId);
+
+	Optional<Participant> findByParticipantId(int participantId);
 }
