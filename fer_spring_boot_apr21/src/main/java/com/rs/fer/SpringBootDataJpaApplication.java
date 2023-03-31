206719f88992e6d15893634b660c421d108e910e
sai28 <sai28@LAPTOP-ND1TKLBI>
2023-03-31 10:45:53 +0530

    identify

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
@@ -9,7 +9,7 @@ public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-		
+		System.out.println("rs");
 	}    
 
 }
