47755864e86e5be1898508809748198a3015450e
ADMIN <ADMIN@DESKTOP-AA6T61H>
2023-03-31 12:26:24 +0530

    Code Updated

---
+++ fer_spring_boot_apr21/src/main/loan/com/rs/fer/loan/response/GetApplicantResponse.java
@@ -0,0 +1,32 @@
+package com.rs.fer.loan.response;
+
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+
+import com.rs.fer.loan.entity.Applicant;
+import com.rs.fer.response.AbstractResponse;
+
+public class GetApplicantResponse extends AbstractResponse {
+
+	public GetApplicantResponse(HttpStatus status, String statusCode, String message, Set<String> errorMessages) {
+		super(status, statusCode, message, errorMessages);
+		this.status = status;
+		this.statusCode = statusCode;
+		this.message = message;
+		this.errorMessages = errorMessages;
+		
+		
+	}
+	private Applicant applicant;
+
+	public Applicant getApplicant() {
+		return applicant;
+	}
+
+	public void setApplicant(Applicant applicant) {
+		this.applicant = applicant;
+	}
+
+
+}
