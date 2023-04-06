70a0477c86b7fae84262ac11bf36c8bbe3a017e8
Nani <Vikaskollu10@gmail.com>
2023-04-06 19:07:16 +0530

    Done With PlayerRepository

---
+++ fer_spring_boot_apr21/src/main/java/Ipl/com/rs/fer/ipl/repository/PlayerRepository.java
@@ -0,0 +1,16 @@
+package com.rs.fer.ipl.repository;
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rs.fer.expense.entity.Expense;
+import com.rs.fer.ipl.entity.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
+
+	
+}
