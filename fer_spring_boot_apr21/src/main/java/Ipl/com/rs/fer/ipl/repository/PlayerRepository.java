b8c3157550a6f5e8c7d50eac1944215598536686
Nani <Vikaskollu10@gmail.com>
2023-04-06 20:16:22 +0530

    Done with playerRepository

--- fer_spring_boot_apr21/src/main/java/Ipl/com/rs/fer/ipl/repository/PlayerRepository.java
+++ fer_spring_boot_apr21/src/main/java/Ipl/com/rs/fer/ipl/repository/PlayerRepository.java
@@ -2,15 +2,15 @@ package com.rs.fer.ipl.repository;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rs.fer.expense.entity.Expense;
 import com.rs.fer.ipl.entity.Player;
+import com.rs.fer.user.entity.User;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
-	
+	List<User> findByJerseyNumber(String jerseyNumber);
+
 }
