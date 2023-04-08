1abf0c6e26f51513cb61e0df5c9078ff82bf9137
DELL E7390 <DELL E7390@DESKTOP-C2I84TP>
2023-04-08 07:59:54 +0530

    code changes are done..

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/ipl/util/impl/PlayerUtilImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/ipl/util/impl/PlayerUtilImplTest.java
@@ -1,63 +1,60 @@
 package com.rs.fer.ipl.util.impl;
 
-	import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	import org.junit.jupiter.api.Test;
-	import org.mockito.InjectMocks;
-	import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
 
-	import com.rs.fer.ipl.entity.Player;
-	import com.rs.fer.ipl.request.SavePlayerRequest;
+import com.rs.fer.ipl.entity.Player;
+import com.rs.fer.ipl.request.SavePlayerRequest;
 
-	@SpringBootTest
-	public class PlayerUtilImplTest {
+@SpringBootTest
+public class PlayerUtilImplTest {
 
-		@InjectMocks
-		private PlayerUtilImpl playerUtilImpl;
-		
-		@Test
-		public void testSavePlayerRequest() {
+	@InjectMocks
+	private PlayerUtilImpl playerUtilImpl;
 
-			SavePlayerRequest request = new SavePlayerRequest();
-			request.setFirstName("abc"); 
-			request.setMiddleName("mno");
-			request.setLastName("xyz");
-			request.setDob("28-09-2022");
-			request.setGender('m');
-			request.setRole("c");
-			request.setSpecilization("AR");
-			request.setJerseyNumber("1");
-			Player player = playerUtilImpl.loadSavePlayerRequestToPlayer(request);
+	@Test
+	public void testSavePlayerRequest() {
 
-			boolean isExpectedNotEmpty = true;
-			boolean isActualNotEmpty = (null != player.getJerseyNumber());
+		SavePlayerRequest request = new SavePlayerRequest();
+		request.setFirstName("abc");
+		request.setMiddleName("mno");
+		request.setLastName("xyz");
+		request.setDob("28-09-2022");
+		request.setGender('m');
+		request.setRole("c");
+		request.setSpecilization("AR");
+		request.setJerseyNumber("1");
+		Player player = playerUtilImpl.loadSavePlayerRequestToPlayer(request);
 
-			assertEquals(isExpectedNotEmpty, isActualNotEmpty);
+		boolean isExpectedNotEmpty = true;
+		boolean isActualNotEmpty = (null != player.getJerseyNumber());
 
-		} 
-		
-		@Test
-		public void testSavePlayerRequestFailure() {
+		assertEquals(isExpectedNotEmpty, isActualNotEmpty);
 
-			SavePlayerRequest request = new SavePlayerRequest();
-			request.setFirstName("abc"); 
-			request.setMiddleName("mno");
-			request.setLastName("xyz");
-			request.setDob("28-09-2022");
-			request.setGender('m');
-			request.setRole("c");
-			request.setSpecilization("AR");
-			//request.setJerseyNumber("1");
-			Player player = playerUtilImpl.loadSavePlayerRequestToPlayer(request);
+	}
 
-			boolean isExpectedNotEmpty = false;
-			boolean isActualNotEmpty = (null != player.getJerseyNumber());
+	@Test
+	public void testSavePlayerRequestFailure() {
 
-			assertEquals(isExpectedNotEmpty, isActualNotEmpty);
+		SavePlayerRequest request = new SavePlayerRequest();
+		request.setFirstName("abc");
+		request.setMiddleName("mno");
+		request.setLastName("xyz");
+		request.setDob("28-09-2022");
+		request.setGender('m');
+		request.setRole("c");
+		request.setSpecilization("AR");
+		// request.setJerseyNumber("1");
+		Player player = playerUtilImpl.loadSavePlayerRequestToPlayer(request);
 
-		}
+		boolean isExpectedNotEmpty = false;
+		boolean isActualNotEmpty = (null != player.getJerseyNumber());
 
+		assertEquals(isExpectedNotEmpty, isActualNotEmpty);
 
-		}
+	}
 
-
+}
