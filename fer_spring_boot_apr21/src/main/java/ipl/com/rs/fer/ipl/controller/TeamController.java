7bd1c6d370bd267c5d46c1260014d80b304e08ad
Admin <Admin@DESKTOP-3JGADQ1.Dlink>
2023-04-10 08:09:44 +0530

    Controller

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/controller/TeamController.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/controller/TeamController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.rs.fer.ipl.validation.TeamValidation;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class TeamController {
 
 	@Autowired
