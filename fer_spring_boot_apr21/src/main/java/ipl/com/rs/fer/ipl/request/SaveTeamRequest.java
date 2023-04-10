0e8ffefe2f7153cb85fc89b999d1f42e870ceb73
Admin <Admin@DESKTOP-3JGADQ1>
2023-04-10 11:56:36 +0530

    Request

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/request/SaveTeamRequest.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/request/SaveTeamRequest.java
@@ -2,18 +2,7 @@ package com.rs.fer.ipl.request;
 
 public class SaveTeamRequest {
 
-	private int teamId;
 	private String name;
-	private String created;
-	private String updated;
-
-	public int getTeamId() {
-		return teamId;
-	}
-
-	public void setTeamId(int teamId) {
-		this.teamId = teamId;
-	}
 
 	public String getName() {
 		return name;
@@ -21,22 +10,6 @@ public class SaveTeamRequest {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getCreated() {
-		return created;
-	}
-
-	public void setCreated(String created) {
-		this.created = created;
-	}
-
-	public String getUpdated() {
-		return updated;
-	}
-
-	public void setUpdated(String updated) {
-		this.updated = updated;
 	}
 
 }
