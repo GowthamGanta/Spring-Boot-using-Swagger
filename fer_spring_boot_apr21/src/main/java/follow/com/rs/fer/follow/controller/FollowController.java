79b62f74496ff18089c297639a2c30a2db3afc1e
kommi <kommi@Komminenis>
2023-04-20 14:40:30 +0530

    removed unused imports

--- fer_spring_boot_apr21/src/main/java/follow/com/rs/fer/follow/controller/FollowController.java
+++ fer_spring_boot_apr21/src/main/java/follow/com/rs/fer/follow/controller/FollowController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +19,6 @@ import com.rs.fer.follow.response.GetFollowersResponse;
 import com.rs.fer.follow.response.SaveFollowerResponse;
 import com.rs.fer.follow.service.FollowerService;
 import com.rs.fer.follow.validation.FollowValidation;
-import com.rs.fer.message.request.DeleteMessageRequest;
-import com.rs.fer.message.response.DeleteMessageResponse;
 
 @RestController
 @RequestMapping("/api")
