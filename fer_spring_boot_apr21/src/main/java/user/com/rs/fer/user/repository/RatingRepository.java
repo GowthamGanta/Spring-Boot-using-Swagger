bcf8f82dfa96959e236614dbdad67004edcf996c
GVR <GVR@DESKTOP-5VPSDDC>
2023-04-20 10:30:24 +0530

    Removed unused imports...

--- fer_spring_boot_apr21/src/main/java/user/com/rs/fer/user/repository/RatingRepository.java
+++ fer_spring_boot_apr21/src/main/java/user/com/rs/fer/user/repository/RatingRepository.java
@@ -1,13 +1,11 @@
 package com.rs.fer.user.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rs.fer.user.entity.Rating;
-import com.rs.fer.user.entity.User;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
