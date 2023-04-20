d4e0586a61107cad57ee61b2a99f040e7e8b9fe8
kommi <kommi@Komminenis>
2023-04-20 16:08:39 +0530

    unused imports

--- fer_spring_boot_apr21/src/main/java/user/com/rs/fer/user/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/user/com/rs/fer/user/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
