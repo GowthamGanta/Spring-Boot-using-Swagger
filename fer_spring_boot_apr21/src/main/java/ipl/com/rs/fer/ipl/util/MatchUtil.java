bcc31223b85b2654e673eee1ee6c7b918b81f832
pavithrajammula <jammulapavithra13@gmail.com>
2023-06-06 18:17:07 +0530

    match util and match utilimpl...

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/util/MatchUtil.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/util/MatchUtil.java
@@ -1,5 +1,9 @@
 package com.rs.fer.ipl.util;
 
-public class MatchUtil {
+import com.rs.fer.ipl.entity.IPLMatch;
+import com.rs.fer.ipl.request.SaveMatchRequest;
+
+public interface MatchUtil {
+	IPLMatch loadSaveMatchRequestToMatch(SaveMatchRequest request);
 
 }
--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/util/impl/MatchUtilImpl.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/util/impl/MatchUtilImpl.java
@@ -1,5 +1,36 @@
 package com.rs.fer.ipl.util.impl;
 
-public class MatchUtilImpl {
+import org.springframework.stereotype.Component;
+
+import com.rs.fer.ipl.entity.IPLMatch;
+import com.rs.fer.ipl.request.SaveMatchRequest;
+import com.rs.fer.ipl.util.MatchUtil;
+import com.rs.fer.util.DateUtil;
+
+@Component
+
+public class MatchUtilImpl implements MatchUtil {
+
+	@Override
+	public IPLMatch loadSaveMatchRequestToMatch(SaveMatchRequest request) {
+		
+		IPLMatch match = new IPLMatch();
+
+		match.setMatchNumber(request.getMatchNumber());
+		match.setMatchDay(request.getMatchDay());
+		match.setTeamOne(request.getTeamOne());
+		match.setTeamTwo(request.getTeamTwo());
+		match.setVenue(request.getVenue());
+		match.setDate(DateUtil.getCurrentDate());
+		match.setStartTime(request.getStartTime());
+		match.setEndTime(request.getEndTime());
+		match.setUmpire(request.getUmpire());
+		match.setThirdUmpire(request.getThirdUmpire());
+		match.setReferee(request.getReferee());
+		match.setHomeTeam(request.getHomeTeam());
+		match.setAwayTeam(request.getAwayTeam());
+
+		return match;
+	}
 
 }
