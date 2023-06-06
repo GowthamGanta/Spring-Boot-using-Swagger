ea6ca91da5f2a309bfa672491cb6479324eed235
gopp6 <gopp6@Gopikrishna-Pc>
2023-06-06 10:54:34 +0530

    Commits on MatchService.java

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/service/MatchService.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/service/MatchService.java
@@ -1,5 +1,18 @@
 package com.rs.fer.ipl.service;
 
-public class MatchService {
+import com.rs.fer.ipl.request.SaveMatchRequest;
+import com.rs.fer.ipl.request.SavePlayerRequest;
+import com.rs.fer.ipl.request.UpdatePlayerRequest;
+import com.rs.fer.ipl.response.DeletePlayerResponse;
+import com.rs.fer.ipl.response.GetPlayerResponse;
+import com.rs.fer.ipl.response.GetPlayersResponse;
+import com.rs.fer.ipl.response.SaveMatchResponse;
+import com.rs.fer.ipl.response.SavePlayerResponse;
+import com.rs.fer.ipl.response.UpdatePlayerResponse;
 
+public interface MatchService {
+
+	
+SaveMatchResponse saveMatch(SaveMatchRequest request);
+	
 }
