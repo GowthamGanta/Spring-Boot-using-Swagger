ca1164787c1a704400592178c1efaef9012036d4
Lavanya <Hp@DESKTOP-443QIOQ>
2023-06-06 11:56:05 +0530

    MatchController...

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/controller/MatchController.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/controller/MatchController.java
@@ -1,5 +1,45 @@
 package com.rs.fer.ipl.controller;
 
-public class  MatchController{
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.rs.fer.ipl.request.SaveMatchRequest;
+import com.rs.fer.ipl.response.SaveMatchResponse;
+import com.rs.fer.ipl.service.MatchService;
+import com.rs.fer.ipl.validation.MatchValidation;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class MatchController {
+	@Autowired
+	MatchValidation matchValidation;
+
+	@Autowired
+	MatchService matchService;
+
+	@PostMapping("/ipl/saveMatch")
+	public SaveMatchResponse saveMatch(@RequestBody SaveMatchRequest request) {
+
+		SaveMatchResponse response = null;
+
+		Set<String> errorMessages = matchValidation.validateSaveMatchRequest(request);
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new SaveMatchResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = matchService.saveMatch(request);
+		}
+
+		return response;
+	}
 
 }
