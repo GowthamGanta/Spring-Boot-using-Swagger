c299675aaaab1f776c56e34eb799ed8b0d8bcbd2
pavithrajammula <jammulapavithra13@gmail.com>
2023-06-06 11:02:24 +0530

    matchRepository...

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/repository/MatchRepository.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/repository/MatchRepository.java
@@ -1,5 +1,15 @@
 package com.rs.fer.ipl.repository;
 
-public class MatchRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rs.fer.ipl.entity.Match;
+
+
+@Repository
+public interface MatchRepository  extends JpaRepository<Match, Integer>{
+	List<Match> findByMatchNumber(int matchNumber);
 
 }
