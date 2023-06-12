ef5232c6f93e9c20dcb69c959e3f082b85853a76
Nani <Vikaskollu10@gmail.com>
2023-06-12 19:36:47 +0530

    Spring Actuator changes

---
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/config/ActuatorSecurity.java
@@ -0,0 +1,19 @@
+package com.rs.fer.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests((requests) ->
+                requests.anyRequest().hasRole("ENDPOINT_ADMIN"));
+        http.httpBasic();
+    }
+ 
+}	
