cda6abe0c9dd0eb4a546ff6e5af9d97136e65c1c
GVR <GVR@DESKTOP-5VPSDDC>
2023-06-24 11:38:04 +0530

    Test 3

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/SpringBootDataJpaApplication.java
@@ -9,7 +9,6 @@ public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-		System.out.println("rs");
 	}    
 
 }
