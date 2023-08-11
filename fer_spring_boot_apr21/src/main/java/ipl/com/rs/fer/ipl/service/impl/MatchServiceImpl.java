83b2ffabd2d5e22b998755f67011275c188f0a90
saikrishna <saikrishna@DESKTOP-7HCN7KB>
2023-08-11 21:17:49 +0530

    Removed unused import

--- fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/service/impl/MatchServiceImpl.java
+++ fer_spring_boot_apr21/src/main/java/ipl/com/rs/fer/ipl/service/impl/MatchServiceImpl.java
@@ -25,7 +25,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import com.rs.fer.ipl.repository.TeamRepository;
 import com.rs.fer.ipl.request.EditMatchRequest;
 import com.rs.fer.ipl.request.SaveMatchRequest;
 import com.rs.fer.ipl.response.EditMatchResponse;
