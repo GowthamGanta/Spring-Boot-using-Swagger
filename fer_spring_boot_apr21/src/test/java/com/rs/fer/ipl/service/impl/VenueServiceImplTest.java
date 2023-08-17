42962213bd1233fd5080dc2164874db2a38f5481
DELL <DELL@DESKTOP-UE202BG>
2023-08-17 15:07:27 +0530

    Commit for saveVenueTestCase

--- fer_spring_boot_apr21/src/test/java/com/rs/fer/ipl/service/impl/VenueServiceImplTest.java
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/ipl/service/impl/VenueServiceImplTest.java
@@ -11,8 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.util.CollectionUtils;
 
 import com.rs.fer.ipl.entity.Venue;
 import com.rs.fer.ipl.repository.VenueRepository;
