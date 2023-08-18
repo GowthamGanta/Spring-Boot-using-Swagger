9df7d167ea1ef3e6c8adb7185c91f3ff8d4b0584
Anusha-Pc <Anusha-Pc@Anusha-Pc>
2023-08-18 11:05:54 +0530

    getVenue test case

---
+++ fer_spring_boot_apr21/src/test/java/com/rs/fer/ipl/service/impl/VenueServiceImplTest.java
@@ -0,0 +1,61 @@
+package com.rs.fer.ipl.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.rs.fer.ipl.entity.Venue;
+import com.rs.fer.ipl.repository.VenueRepository;
+import com.rs.fer.ipl.response.GetVenuesResponse;
+import com.rs.fer.ipl.util.VenueUtil;
+@SpringBootTest
+public class VenueServiceImplTest {
+	
+	@InjectMocks
+	private VenueServiceImpl venueServiceImpl;
+
+	@Mock
+	VenueRepository venueRepository;
+
+	@Mock
+	VenueUtil venueUtil;
+
+	// private Object teams;
+	public void testGetVenue() {
+		List<Venue> venue = new ArrayList<>();
+        
+        ((Venue) venue).setStadiumName("LBstadium");
+        
+        when(venueRepository.findByStadiumName(Mockito.anyString())).thenReturn(venue);
+        
+        String StadiumName="LBStadium";
+        GetVenuesResponse response=venueServiceImpl.getVenues();
+        
+        assertEquals("000", response.statusCode);
+        		
+	}
+	@Test
+	public void testGetVenueNotFound() {
+		List<Venue> venue = new ArrayList<>();
+
+		// Team teams = new Team();
+		// Mock
+		when(venueRepository.findByStadiumName(Mockito.anyString())).thenReturn(venue);
+
+		// 2.
+		String StadiumName = null;
+		GetVenuesResponse response=venueServiceImpl.getVenues();
+		
+
+		// 3.
+		assertEquals("002", response.statusCode);
+	}
+}
